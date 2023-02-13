5855212ddce35b502e2108a75ca49e6352a870d4
aaronwhitemkc <aaron_white@mckinsey.com>
2023-02-13 15:43:28 -0500

    Remove S3 Bucket

--- main.tf
+++ main.tf
@@ -10,9 +10,7 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
-resource "aws_s3_bucket" "lambda_zip" {
-  bucket = var.aws_s3_bucket
-}
+
 
 resource "aws_dynamodb_table" "questions" {
   name           = "questions"
