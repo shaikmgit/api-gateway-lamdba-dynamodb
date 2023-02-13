39826c45329137c1d7d9fc02b44d789488f2c4dc
aaronwhitemkc <aaron_white@mckinsey.com>
2023-02-13 15:12:45 -0500

    S3 Bucket

--- main.tf
+++ main.tf
@@ -10,8 +10,6 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
-
-
 resource "aws_s3_bucket" "lambda_zip" {
   bucket = var.aws_s3_bucket
 }
--- variables.tf
+++ variables.tf
@@ -5,6 +5,7 @@ variable "jwtSecret" {
 
 variable "aws_region" {
   type    = string
+  default = "us-west-1"
 }
 
 variable "aws_s3_bucket" {
